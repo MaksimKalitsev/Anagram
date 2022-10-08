4c2109e900f66b7a5faf51b39a1199b5ddaf98c5
Maksim.Kalitsev <maksim.kalitsev@ukr.net>
2022-10-08 17:44:58 +0300

    fixed some bugs, code stopping worked

--- app/src/main/java/ua/com/foxminded/anagrams/MainActivity.java
+++ app/src/main/java/ua/com/foxminded/anagrams/MainActivity.java
@@ -1,9 +1,6 @@
 package ua.com.foxminded.anagrams;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,33 +9,32 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button convert_btn;
+    EditText sentence_et;
+    EditText ignoredSymbols_et;
+    TextView result_tv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.convert);
-        EditText editText = findViewById(R.id.Type_a_word);
-        EditText editTextIgnore = findViewById(R.id.Symbol_to_ignore);
-        TextView textView = findViewById(R.id.result);
-        button.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.N)
-            @Override
+        convert_btn = findViewById(R.id.convert);
+        sentence_et = findViewById(R.id.sentence);
+        ignoredSymbols_et = findViewById(R.id.ignoredSymbols);
+        result_tv = findViewById(R.id.result);
+        convert_btn.setOnClickListener(new View.OnClickListener() {
+
+
             public void onClick(View view) {
-                if(editText.getText().toString().trim().equals(""))
+                if (sentence_et.getText().toString().trim().equals("")) {
                     Toast.makeText(MainActivity.this, R.string.EmptyString, Toast.LENGTH_SHORT).show();
-                else {
-                    Anagram anagram = new Anagram();
-                    String text = editText.getText().toString();
-                 //   textView.setText(anagram.makeAnagram(text));
-
-                    String ignoreSymbols = editTextIgnore.getText().toString();
-                    textView.setText(anagram.makeAnagram(text,ignoreSymbols));
-
+                } else {
+                    String text = sentence_et.getText().toString();
+                    String ignoreSymbols = ignoredSymbols_et.getText().toString();
+                    result_tv.setText(StringUtils.makeAnagram(text, ignoreSymbols));
                 }
-
             }
         });
-
     }
 }
---
+++ app/src/main/java/ua/com/foxminded/anagrams/StringUtils.java
@@ -0,0 +1,65 @@
+package ua.com.foxminded.anagrams;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+final public class StringUtils {
+    private StringUtils() {
+    }
+
+    public static String makeAnagram(String sentence, String ignoreString) {
+        if (sentence == null || sentence.isEmpty()) {
+            return "";
+        }
+        final String SPACE_DELIMITER = " ";
+        String[] words = sentence.split(SPACE_DELIMITER, sentence.length());
+        StringBuilder result = new StringBuilder(SPACE_DELIMITER);
+        Set<Character> ignoredSymbols = stringToSet(ignoreString);
+
+        for (String part : words) {
+            result.append(reversedWord(part, ignoredSymbols));
+        }
+        return result.toString();
+    }
+
+    private static Set<Character> stringToSet(String ignoreSymbols) {
+        Set<Character> ignore = new HashSet<>();
+        for (char symbol : ignoreSymbols.toCharArray()) {
+            ignore.add(symbol);
+        }
+        return ignore;
+    }
+
+    private static boolean shouldBeProcessed(char symbol, Set<Character> ignoredSymbols) {
+        if (ignoredSymbols.isEmpty()) {
+            return Character.isLetter(symbol);
+        } else {
+            return !ignoredSymbols.contains(symbol);
+        }
+    }
+
+    private static String reversedWord(String word, Set<Character> ignored) {
+        char[] result = word.toCharArray();
+        int i = 0;
+        int j = result.length - 1;
+        char temp;
+
+        while (i < j) {
+            temp = result[i];
+            result[i] = result[j];
+            result[j] = temp;
+            i++;
+            j--;
+            if (!shouldBeProcessed(result[i], ignored) && shouldBeProcessed(result[j], ignored)) {
+                i++;
+            } else if (shouldBeProcessed(result[i], ignored) && !shouldBeProcessed(result[j], ignored)) {
+                j--;
+            } else {
+                i++;
+                j--;
+            }
+        }
+        return new String(result);
+    }
+}
