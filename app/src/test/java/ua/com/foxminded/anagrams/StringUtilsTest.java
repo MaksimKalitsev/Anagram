dbc7df810c39d07ee751af3f8fee5eeeb4fb57be
Maksim.Kalitsev <maksim.kalitsev@ukr.net>
2022-11-08 20:25:39 +0200

    testMultiLine failed

--- app/src/test/java/ua/com/foxminded/anagrams/StringUtilsTest.java
+++ app/src/test/java/ua/com/foxminded/anagrams/StringUtilsTest.java
@@ -10,21 +10,36 @@ public class StringUtilsTest {
 
     @Test
     public void testOnlyLetters() {
-        assertEquals("abcd efgh", StringUtils.makeAnagram("dcba hgfe", ""));
+        assertEquals("dcba hgfe", StringUtils.makeAnagram("abcd efgh", ""));
     }
 
     @Test
     public void testLettersAndSymbols() {
-        assertEquals("a1bcd efg!h", StringUtils.makeAnagram("d1cba hgf!e", ""));
+        assertEquals("d1cba hgf!e", StringUtils.makeAnagram("a1bcd efg!h", ""));
     }
 
     @Test
     public void testOnlyLettersWithFilter() {
-        assertEquals("abcd efgh", StringUtils.makeAnagram("dcba hgfe", "xl"));
+        assertEquals("dcba hgfe", StringUtils.makeAnagram("abcd efgh", "xl"));
     }
 
     @Test
     public void testLettersAndSymbolsWithFilter() {
-        assertEquals("Foxminded cool 24/7", StringUtils.makeAnagram("dexdnimoF oocl 7/42", "xl"));
+        assertEquals("dexdnimoF oocl 7/42", StringUtils.makeAnagram("Foxminded cool 24/7", "xl"));
+    }
+
+    @Test
+    public void testEmptyString() {
+        assertEquals("", StringUtils.makeAnagram("", ""));
+    }
+
+    @Test
+    public void testEmptyStringWithFilter() {
+        assertEquals("", StringUtils.makeAnagram("", "xl"));
+    }
+
+    @Test
+    public void testMultiLine() {
+        assertEquals("qwerty\nasdfgh", StringUtils.makeAnagram("ytrewq\nhgfdsa", ""));
     }
 }
